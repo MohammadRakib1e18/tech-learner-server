11a38e772ba2e73f590610abe90b3e67655eed6d
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-27 08:13:54 +0600

    fake data added

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const categories = require('./data/categories.json');
+const courses    = require('./data/courses.json');
 
-app.get('/', (req, res)=>{
-    res.send('tech-learner server is running')
+app.get('/categories', (req, res)=>{
+  res.send(categories);
+})
+
+app.get('/courses', (req, res)=>{
+  res.send(courses);
 })
 
 app.listen(port, () => {
