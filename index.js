e703b80f645c3fa7f81620de4ae92d3724e2ad5a
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-10-27 08:38:38 +0600

    category with id based data API created

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-const categories = require('./data/categories.json');
-const courses    = require('./data/courses.json');
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
-app.get('/categories', (req, res)=>{
+app.get("/categories", (req, res) => {
   res.send(categories);
-})
+});
 
-app.get('/courses', (req, res)=>{
+app.get("/courses", (req, res) => {
   res.send(courses);
-})
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+
+  const category_courses = courses.filter((n) => n.category_id === id);
+  console.log(category_courses);
+  res.send(category_courses);
+});
 
 app.listen(port, () => {
   console.log("Tech Learner Server running on port", port);
